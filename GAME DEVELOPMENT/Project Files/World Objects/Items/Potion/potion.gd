5d865dee7cc2faa891966b33fae70ffe274396d0
Arnold <arniethepie@gmail.com>
2021-12-19 03:40:08 +0000

    Merged some levels, fixed level construction and design
    Level 1A 2A 3A 4A 5A complete

--- GAME DEVELOPMENT/Project Files/World Objects/Items/Potion/potion.gd
+++ GAME DEVELOPMENT/Project Files/World Objects/Items/Potion/potion.gd
@@ -3,6 +3,7 @@ extends Area2D
 onready var anim_player: AnimationPlayer = get_node("CollisionShape2D/AnimationPlayer")
 
 # when body entered, tell the game that a potion has been picked up
-func _on_body_entered(body: PhysicsBody2D) -> void:
-	Eventbus.emit_signal("potionpickup")
-	anim_player.play("fade_out")
+func _on_body_entered(body):
+	if body.get("TYPE") == "player":
+		Eventbus.emit_signal("potionpickup")
+		anim_player.play("fade_out")
